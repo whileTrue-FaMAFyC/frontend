012ea6d0c5365b9b861b9811c9c466b88574c88f
Rocolog <lucas.cassina@outlook.com>
2022-10-12 17:03:18 -0300

    PYR-9 Registro estilizado

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,6 +1,14 @@
 import {useForm} from "react-hook-form";
 import {useState} from "react";
-import "./reg.css";
+
+import {
+  StyledButton,
+  StyledEntryCard,
+  StyledInput,
+  StyledInputGroup,
+  EntryPage,
+  StyledError,
+} from "./Reg.style";
 
 const Formulario = () => {
   const {
@@ -23,128 +31,130 @@ const Formulario = () => {
   };
 
   return (
-    <div className='registro'>
-      <h1>Registro</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label className='form-label' htmlFor='inputUsername'>
-            Username
-          </label>
-          <input
-            type='text'
-            id='inputUsername'
-            {...register("username", {
-              required: true,
-              maxLength: 16,
-              minLength: 3,
-            })}
-          />
-          {errors.username?.type === "required" && (
-            <p class='error'>Ingrese un usuario</p>
-          )}
-          {errors.username?.type === "maxLength" && (
-            <p class='error'>
-              El campo username puede tener a lo sumo 16 caracteres
-            </p>
-          )}
-          {errors.username?.type === "minLength" && (
-            <p class='error'>
-              El campo username debe tener al menos 3 caracteres
-            </p>
-          )}
-        </div>
-        <div>
-          <label className='form-label' htmlFor='inputEmail'>
-            Email
-          </label>
-          <input
-            type='text'
-            id='inputEmail'
-            {...register("email", {
-              required: true,
-              pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
-            })}
-          />
-          {errors.email?.type === "pattern" && (
-            <p class='error'>El formato del email es incorrecto</p>
-          )}
-          {errors.email?.type === "required" && (
-            <p class='error'>Ingrese un email</p>
-          )}
-        </div>
-        <div>
-          <label className='form-label' htmlFor='inputPassword'>
-            Password
-          </label>
-          <input
-            type='password'
-            id='inputPassword'
-            {...register("password", {
-              required: true,
-              pattern: /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})$/,
-            })}
-          />
-          {errors.password?.type === "pattern" && (
-            <p class='error'>
-              La contraseña debe contener al menos una mayuscula, minuscula y
-              numero
-            </p>
-          )}
-          {errors.password?.type === "required" && (
-            <p class='error'>Ingrese una contraseña</p>
-          )}
-        </div>
-        <div>
-          <label className='form-label' htmlFor='inputConfirmPassword'>
-            Confirm password
-          </label>
-          <input
-            type='password'
-            id='inputConfirmPassword'
-            {...register("confirmPassword", {
-              required: true,
-              validate: (val) => {
-                if (watch("password") !== val) {
-                  return false;
-                }
-              },
-            })}
-          />
-          {errors.confirmPassword?.type === "validate" && (
-            <p class='error'>Las contraseñas no coinciden</p>
-          )}
-          {errors.email?.type === "required" && (
-            <p class='error'>Ingrese nuevamente su contraseña</p>
-          )}
-        </div>
-        <div>
-          <label className='form-label' htmlFor='inputAvatar'>
-            Avatar
-          </label>
-          <input
-            type='file'
-            id='inputAvatar'
-            accept='.png'
-            {...register("avatar", {
-              validate: (val) => {
-                return val.length === 0 || val[0].type === "image/png";
-              },
-            })}
-          />
-          {errors.avatar?.type === "validate" && (
-            <p class='error'>
-              La extension del archivo es incorrecta, el archivo debe ser .png
-            </p>
-          )}
-        </div>
-        <button type='submit'>Enviar</button>
-      </form>
-      {success && (
-        <div className='alert alert-success mt-4' role='alert'>
-          Se mandó la solicitud de registro
-        </div>
-      )}
-    </div>
+    <StyledEntryCard>
+      <EntryPage className='registro'>
+        <h1>Registro</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <StyledInputGroup>
+            <label className='form-label' htmlFor='inputUsername'>
+              Username
+            </label>
+            <StyledInput
+              type='text'
+              id='inputUsername'
+              {...register("username", {
+                required: true,
+                maxLength: 16,
+                minLength: 3,
+              })}
+            />
+            {errors.username?.type === "required" && (
+              <StyledError>Ingrese un usuario</StyledError>
+            )}
+            {errors.username?.type === "maxLength" && (
+              <StyledError>
+                El campo username puede tener a lo sumo 16 caracteres
+              </StyledError>
+            )}
+            {errors.username?.type === "minLength" && (
+              <StyledError>
+                El campo username debe tener al menos 3 caracteres
+              </StyledError>
+            )}
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <label className='form-label' htmlFor='inputEmail'>
+              Email
+            </label>
+            <StyledInput
+              type='text'
+              id='inputEmail'
+              {...register("email", {
+                required: true,
+                pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+              })}
+            />
+            {errors.email?.type === "pattern" && (
+              <StyledError>El formato del email es incorrecto</StyledError>
+            )}
+            {errors.email?.type === "required" && (
+              <StyledError>Ingrese un email</StyledError>
+            )}
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <label className='form-label' htmlFor='inputPassword'>
+              Password
+            </label>
+            <StyledInput
+              type='password'
+              id='inputPassword'
+              {...register("password", {
+                required: true,
+                pattern: /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20})$/,
+              })}
+            />
+            {errors.password?.type === "pattern" && (
+              <StyledError>
+                La contraseña debe contener al menos una mayuscula, minuscula y
+                numero
+              </StyledError>
+            )}
+            {errors.password?.type === "required" && (
+              <StyledError>Ingrese una contraseña</StyledError>
+            )}
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <label className='form-label' htmlFor='inputConfirmPassword'>
+              Confirm password
+            </label>
+            <StyledInput
+              type='password'
+              id='inputConfirmPassword'
+              {...register("confirmPassword", {
+                required: true,
+                validate: (val) => {
+                  if (watch("password") !== val) {
+                    return false;
+                  }
+                },
+              })}
+            />
+            {errors.confirmPassword?.type === "validate" && (
+              <StyledError>Las contraseñas no coinciden</StyledError>
+            )}
+            {errors.email?.type === "required" && (
+              <StyledError>Ingrese nuevamente su contraseña</StyledError>
+            )}
+          </StyledInputGroup>
+          <StyledInputGroup>
+            <label className='form-label' htmlFor='inputAvatar'>
+              Avatar
+            </label>
+            <StyledInput
+              type='file'
+              id='inputAvatar'
+              accept='.png'
+              {...register("avatar", {
+                validate: (val) => {
+                  return val.length === 0 || val[0].type === "image/png";
+                },
+              })}
+            />
+            {errors.avatar?.type === "validate" && (
+              <StyledError>
+                La extension del archivo es incorrecta, el archivo debe ser .png
+              </StyledError>
+            )}
+          </StyledInputGroup>
+          <StyledButton type='submit'>Enviar</StyledButton>
+        </form>
+        {success && (
+          <StyledInputGroup className='alert alert-success mt-4' role='alert'>
+            Se mandó la solicitud de registro
+          </StyledInputGroup>
+        )}
+      </EntryPage>
+    </StyledEntryCard>
   );
 };
 
