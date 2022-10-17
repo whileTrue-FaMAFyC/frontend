c1d3cc7774f1af628267dc9133ef4775916762cf
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2022-10-17 13:09:23 -0300

    Rename mocks

---
+++ src/__mocks__/handlers.js
@@ -0,0 +1,7 @@
+import {rest} from "msw";
+
+export const handlers = [
+  rest.post(`${process.env.REACT_APP_API_KEY}`, (req, res, ctx) => {
+    return res(ctx.json({status: 200, success: true}));
+  }),
+];
