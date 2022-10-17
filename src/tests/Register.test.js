fabb50944eeed43641c0f86028cfcce7521a87c3
Rocolog <lucas.cassina@outlook.com>
2022-10-17 19:57:32 -0300

    PYR-9 TAMOSSSS

--- src/tests/Register.test.js
+++ src/tests/Register.test.js
@@ -1,6 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import Formulario from "../components/Register";
+import Formulario from "../components/Register/Register";
 import {server} from "../__mocks__/server.js";
 import {rest} from "msw";
 
