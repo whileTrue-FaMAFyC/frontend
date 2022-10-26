f859171230bd580b4f6e5f702b339701acfa464b
Sebastián José Giraudo <seba.jose.giraudo@mi.unc.edu.ar>
2022-10-26 17:42:02 -0300

    PYR-90 primera implementacion de barra de vida

--- src/components/BotInGame/BotInGame.jsx
+++ src/components/BotInGame/BotInGame.jsx
@@ -1,8 +1,9 @@
 import {ReactComponent as Bot} from "./robot-line.svg";
 import {motion} from "framer-motion";
 import {EntryPage, StyledBoard} from "./Board.style";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useCycle} from "framer-motion";
+import ProgressBar from "./StatusBar";
 
 const BotInGame = () => {
   const [animation, cycleAnimation] = useCycle("animationOne");
@@ -12,6 +13,10 @@ const BotInGame = () => {
       cycleAnimation();
     }, 1000);
   }, [cycleAnimation]);
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
 
   const PlaneVariants = {
     animationOne: {
@@ -23,8 +28,11 @@ const BotInGame = () => {
     },
   };
 
+  const [completed, setCompleted] = useState(0);
+
   return (
     <EntryPage>
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
       <StyledBoard data-testid='board'>
         <motion.div variants={PlaneVariants} animate={animation}>
           <Bot data-testid='robot' />
