346127e41236052ba9dd287dc73063be5adbaf61
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2022-10-27 12:09:02 -0300

    PYR-104 primer alternativa: CSS transition

--- src/components/BotInGame/Board.style.js
+++ src/components/BotInGame/Board.style.js
@@ -1,18 +1,24 @@
 import styled from "styled-components";
 
 export const StyledBoard = styled.div`
-  display: flex;
-  align-items: flex-start;
-  justify-content: stretch;
+  position: relative;
   margin-top: 50px;
   width: 500px;
   height: 500px;
-  padding: 30px;
+  //padding: 30px;
   color: #bf7636;
   background-color: #f2be5c;
   border: 5px inset #bb7032;
   border-radius: 5px;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+export const Robot = styled.div`
+  position: absolute;
+  left: ${(props) => `${props.coordinates.x}%`};
+  top: ${(props) => `${props.coordinates.y}%`};
+  border-radius: inherit;
+  transition: 1s ease-in-out;
 `;
 
 export const EntryPage = styled.div`
