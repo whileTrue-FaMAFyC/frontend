cb261b13cc7f0930738c69c4461ece0bb58b2551
Rocolog <lucas.cassina@outlook.com>
2022-10-29 18:27:41 -0300

    Diseño 2 (No centrado, sin esfuerzo)

--- src/components/RobotsLibrary/RobotsLibrary.jsx
+++ src/components/RobotsLibrary/RobotsLibrary.jsx
@@ -1,22 +1,19 @@
 import {useState, useEffect} from "react";
-
-import {styled} from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import ButtonBase from "@mui/material/ButtonBase";
 import {getRobotsNames} from "../../services";
 import Avatar from "@mui/material/Avatar";
-import Paper from "@mui/material/Paper";
 import {
-  StyledButton,
+  Container,
+  Title,
+  Table,
+  Thead,
+  Column,
+  Tbody,
+  Row,
+  Feedback,
   StyledEntryCard,
-  StyledInput,
-  StyledInputGroup,
-  EntryPage,
-  StyledError,
 } from "./RobotsLibrary.style";
 
-const ShowRobots = () => {
+const RobotsLibrary = () => {
   const [robotsNames, setRobotsNames] = useState([]);
 
   const callGetRobotsNames = async () => {
@@ -32,59 +29,23 @@ const ShowRobots = () => {
     callGetRobotsNames();
     console.log(robotsNames);
   }, []);
-
   return (
-    <EntryPage>
-      <h2> Library </h2>
-      {robotsNames.map((robot) => (
-        <Paper
-          sx={{
-            p: 2,
-            margin: "auto",
-            borderRadius: 2,
-            maxWidth: 500,
-            flexGrow: 1,
-            backgroundColor: "#252c32",
-            maxWidth: 800,
-            maxHeight: 150,
-            margin: `2px auto`,
-            padding: 2,
-          }}>
-          <Grid container spacing={2} key={robot.name}>
-            <Grid item>
-              <Avatar
-                style={{
-                  height: 120,
-                  width: 120,
-                  verticalAlign: "middle",
-                  position: "relative",
-                  justifyContent: "center",
-                }}
-                spacing={2}
-                src={robot.avatar}
-              />
-            </Grid>
-            <Grid
-              item
-              xs
-              container
-              spacing={0}
-              direction='column'
-              alignItems='center'
-              justifyContent='center'>
-              <Typography
-                gutterBottom
-                variant='body1'
-                color='common.white'
-                style={{justifyContent: "center"}}>
-                {robot.name}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Paper>
-      ))}
-    </EntryPage>
+    <StyledEntryCard>
+      <Title>Game list</Title>
+      <Table>
+        <Tbody>
+          {robotsNames.map(({avatar, name}) => (
+            <Row key={name} data-testid='row'>
+              <Column>
+                {" "}
+                <Avatar spacing={2} src={avatar} />
+              </Column>
+              <Column>{name}</Column>
+            </Row>
+          ))}
+        </Tbody>
+      </Table>
+    </StyledEntryCard>
   );
 };
-
-export default ShowRobots;
+export default RobotsLibrary;
--- src/components/RobotsLibrary/RobotsLibrary.style.js
+++ src/components/RobotsLibrary/RobotsLibrary.style.js
@@ -1,31 +1,8 @@
-import styled, {createGlobalStyle} from "styled-components";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-
-export const StyledButton = styled.button`
-  width: ${(props) => (props.full ? "100%" : null)};
-  min-width: 64px;
-  border: 0;
-  border-radius: 4px;
-  padding: 8px 16px;
-  outline: none;
-  background-color: #00c8c8;
-  //color: #252c32;
-  font-size: 0.875rem;
-  font-weight: 500;
-  line-height: 1.5;
-  letter-spacing: 0.02857rem;
-  cursor: pointer;
-  transition: all 0.2s;
-  &:hover {
-    background-color: #009595;
-  }
-`;
+import styled from "styled-components";
 
 export const StyledEntryCard = styled.div`
   width: 100%;
-  max-width: 450px;
-  border-radius: 5px;
+  height: 100%;
   padding: 50px;
   margin-bottom: 40px;
   background-color: #252c32;
@@ -47,76 +24,108 @@ export const StyledEntryCard = styled.div`
   }
 `;
 
-export const StyledInputGroup = styled.div`
-  margin-bottom: 24px;
-  text-align: left;
-  label {
-    display: inline-block;
-    margin-bottom: 0.5rem;
-    color: #fff;
-  }
-`;
-
-export const StyledInput = styled.input`
-  width: 100%;
-  outline: none;
-  padding: 8px 16px;
-  border: 1px solid #00c8c8;
-  border-radius: 4px;
-  font-size: 1rem;
-  -webkit-text-fill-color: #dedede;
-  transition: box-shadow 0.2s;
-  background-color: #252c32;
-  &::placeholder {
-    color: blue;
-    -webkit-text-fill-color: #b8b8b8;
-  }
-  &:focus {
-    box-shadow: 0 0 0 2px rgb(0, 200, 200, 0.5);
-    color: #0f1519;
-  }
-`;
-
-export const StyledError = styled.p`
-  font-size: 12px;
-  padding: 3px;
-  color: #be0000;
-`;
-
-export const StyledSuccess = styled.p`
-  font-size: 12px;
-  padding: 3px;
-  color: green;
-`;
-
-export const EntryPage = styled.div`
-  align-items: center;
+export const Container = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   flex-direction: column;
-  min-height: 100vh;
+  gap: 20px;
+`;
+
+export const Title = styled.p`
+  color: #fff;
+  font-size: 30px;
+  font-family: "Colfax";
+`;
+
+export const Table = styled.table`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
   background-color: #0f1519;
-  text-align: center;
-  padding: 30px;
-  h2 {
-    font-weight: 500;
-    top: 150px;
-    margin-bottom: 30px;
+`;
+
+export const Thead = styled.thead`
+  width: 100%;
+  min-height: 60px;
+  display: flex;
+  background-color: #000;
+
+  tr {
+    &:hover {
+      background-color: #000;
+      cursor: auto;
+    }
   }
 `;
 
-export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        text-decoration: none;
-        box-sizing: border-box;
-    }
+export const Column = styled.td`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Colfax";
+`;
 
-    body {
-        font-size: 16px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        color: #333;
-        -webkit-font-smoothing: antialised;
-        -moz-osx-font-smoothing: grayscale;
-    }
+export const Tbody = styled.tbody`
+  width: 100%;
+  height: 100%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  border: 1px;
+  padding: 0 5px;
+
+  &::-webkit-scrollbar {
+    appearance: none;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #252c32;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    width: 10px;
+    margin: 3px 0px;
+  }
+
+  &::-webkit-scrollbar:vertical {
+    width: 8px;
+  }
+`;
+
+export const Row = styled.tr`
+  width: 100%;
+  min-height: 60px;
+  display: flex;
+  border-bottom: 1px solid #252c32;
+  color: #fff;
+  &:hover {
+    background-color: #ff4655;
+    background-color: #0a0e13;
+  }
+  cursor: pointer;
+`;
+
+export const Feedback = styled.tr`
+  width: 100%;
+  min-height: 100%;
+  display: flex;
+  color: #fff;
+`;
+
+export const Ellipsis = styled.p`
+  max-width: 120px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+export const Refresh = styled.button`
+  width: 300px;
+  height: 25px;
 `;
