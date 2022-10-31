47184f6b54b6c32d8e7690a1b75d590eef66121a
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2022-10-31 19:41:39 -0300

    Arreglo estilo y nombre de styled

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,30 +1,30 @@
-import {WelcomeImg, StyledButton, StyledEntryCard} from "./Welcome.styled";
-import {Link} from "react-router-dom";
+import {WelcomeImg, StyledLink, StyledEntryCard} from "./Welcome.styled";
 
 const Welcome = () => {
   return (
     <WelcomeImg
       style={{
-        backgroundImage: `url("https://robocraftgame.com/images/devblog/robocraftgen_banner.jpg")`,
-        color: "#E1B338",
+        backgroundImage: `url("https://imgs.search.brave.com/VeYbVrPVN-lcUrB1ADmGaK2TGhepR0WIA8xKEIUav9E/rs:fit:1200:1200:1/g:ce/aHR0cDovL3dhbGxw/YXBlcmNhdmUuY29t/L3dwL2xXUzlyOGMu/anBn")`,
       }}
       data-testid='home'>
+      <img
+        src='https://imgs.search.brave.com/d0N5nVGQrq83hlOQGSUv8eOzavubmuFWncmj3cdPYbk/rs:fit:658:747:1/g:ce/aHR0cHM6Ly9mcmVl/cG5naW1nLmNvbS90/aHVtYi90ZWNobm9s/b2d5LzcyOTY3LXRo/cmVlLWRpbWVuc2lv/bmFsLXNwYWNlLXJv/Ym90aWNzLXJvYm90/LWNvbXB1dGVyLWdy/YXBoaWNzLWJvcmRl/ci5wbmc'
+        width={150}
+        height={150}
+        style={{position: "absolute", left: "595px"}}
+      />
       <StyledEntryCard>
         <h2>PyRobots</h2>
-        <div>
-          <StyledButton>
-            <Link to='/login' data-testid='linkToLogin'>
-              Login
-            </Link>
-          </StyledButton>
-        </div>
-        <div>
-          <StyledButton>
-            <Link to='/register' data-testid='linkToReg'>
-              Register
-            </Link>
-          </StyledButton>
-        </div>
+        {/* <StyledButton type='button'> */}
+        <StyledLink to='/login' data-testid='linkToLogin'>
+          Login
+        </StyledLink>
+        {/* </StyledButton> */}
+        {/* <StyledButton type='button'> */}
+        <StyledLink to='/register' data-testid='linkToReg'>
+          Register
+        </StyledLink>
+        {/* </StyledButton> */}
       </StyledEntryCard>
     </WelcomeImg>
   );
