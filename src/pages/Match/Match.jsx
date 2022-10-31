cd686c55fa04cb3048c70ed85bd82e9d1595085f
bas-benja <benjamin.bas@mi.unc.edu.ar>
2022-10-31 16:13:36 -0300

    PYR-82_websocket-working

--- src/pages/Match/Match.jsx
+++ src/pages/Match/Match.jsx
@@ -1,28 +1,61 @@
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
 import useMatch from "../../hooks/useMatch";
-import {getMatchInfo} from "../../services";
-import {useParams} from "react-router-dom";
-import {Container, Button} from "./Match.styled";
+import { getMatchInfo } from "../../services";
+import { useParams } from "react-router-dom";
+import { Container, Button } from "./Match.styled";
 
 const Match = () => {
-  const {id} = useParams();
-  const {match, dispatch} = useMatch();
+  const { id } = useParams();
+  const { match, dispatch } = useMatch();
+  // const [socket, setSocket] = useState(
+  //   new WebSocket(`process.env.REACT_APP_WEB_SOCKET${id}?
+  //     authorization=${localStorage.getItem("user")}`)
+  // );
 
-  const callGetMatchInfo = async () => {
-    try {
-      const response = await getMatchInfo(localStorage.getItem("user"), id);
-      dispatch({type: "initial_info", payload: response.data});
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const ws = new WebSocket(`${process.env.REACT_APP_WEB_SOCKET}${id}?authorization=${localStorage.getItem("user")}`);
+  ws.onopen = () => ws.send("WEBSOCKET CONNECTED");
+
+  // const callGetMatchInfo = async () => {
+  //   try {
+  //     const response = await getMatchInfo(localStorage.getItem("user"), id);
+  //     dispatch({ type: "initial_info", payload: response.data });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   useEffect(() => {
+    const callGetMatchInfo = async () => {
+      try {
+        const response = await getMatchInfo(localStorage.getItem("user"), id);
+        dispatch({ type: "initial_info", payload: response.data });
+      } catch (error) {
+        console.log(error);
+      }
+    };
     callGetMatchInfo();
   }, []);
 
   return (
     <Container>
+      <p>
+        {match.name}
+      </p>
+      <p>
+        {match.num_games}
+      </p>
+      <p>
+        {match.num_rounds}
+      </p>
+      <p>
+        {match.min_players}
+      </p>
+      <p>
+        {match.max_players}
+      </p>
+      <p>
+        {match.users_joined}
+      </p>
       {Object.keys(match.user_robot).map((key, index) => (
         <p key={index}>
           {key}
@@ -35,9 +68,7 @@ const Match = () => {
 };
 export default Match;
 
-/*   const [socket, setSocket] = useState(
-    new WebSocket(process.env.REACT_APP_WEB_SOCKET)
-  ); */
+
 
 /*  useEffect(() => {
     socket.onmessage = (e) => {
