5c09b21fdecfe76183b7e0f4872f1f6c6df6f54a
Rocolog <lucas.cassina@outlook.com>
2022-11-16 17:15:07 -0300

    PYR-135 Filtrado funcionando y tests

--- src/components/RobotsLibrary/RobotsLibrary.jsx
+++ src/components/RobotsLibrary/RobotsLibrary.jsx
@@ -1,17 +1,20 @@
 import {useState, useEffect} from "react";
 import {getRobotsNames} from "../../services";
 import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
 import {Link} from "react-router-dom";
 import {
-  Table,
+  PadreContainer,
+  Container,
   Title,
+  Table,
+  Thead,
   Column,
   Tbody,
   Row,
   Feedback,
-  StyledEntryCard,
   StyledButton,
-  EntryPage,
+  StyledInput,
 } from "./RobotsLibrary.style";
 
 const RobotsLibrary = () => {
@@ -21,7 +24,9 @@ const RobotsLibrary = () => {
   const callGetRobotsNames = async () => {
     try {
       const response = await getRobotsNames(localStorage.getItem(`user`));
-      setRobotsNames(response.data);
+      if (response.data.length) {
+        setRobotsNames(response.data);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -32,15 +37,45 @@ const RobotsLibrary = () => {
   }, []);
 
   return (
-    <EntryPage>
-      <StyledEntryCard>
-        <Title data-testid='title'>My robots</Title>
+    <PadreContainer>
+      <Container>
+        <Title style={{marginBottom: -25}} data-testid='title'>
+          My robots
+        </Title>
+        <StyledInput
+          type='text'
+          data-testid='filter'
+          placeholder='Find your robot...'
+          style={{
+            height: 30,
+            verticalAlign: "middle",
+            position: "relative",
+            justifyContent: "center",
+            top: "20px",
+          }}
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+        />
+        {/* <TextField
+          fullWidth
+          label='Search'
+          id='fullWidth'
+          placeholder='Find your robot...'
+          size='small'
+          onChange={(e) => setQuery(e.target.value.toLowerCase())}
+          sx={{backgroundColor: "#00c8c8"}}
+        /> */}
         <Table>
-          <input
-            type='text'
-            placeholder='Find your robot...'
-            onChange={(e) => setQuery(e.target.value.toLowerCase())}
-          />
+          <Thead>
+            <Row>
+              <Column sortable='true'>Avatar</Column>
+              <Column>Name</Column>
+              <Column>Matches played</Column>
+              <Column>Matches won</Column>
+              <Column>Matches tied</Column>
+              <Column>Matches lost</Column>
+              <Column>Games win rate</Column>
+            </Row>
+          </Thead>
           <Tbody>
             {robotsNames.length < 1 && (
               <Feedback>
@@ -51,24 +86,31 @@ const RobotsLibrary = () => {
               .filter((robotName) =>
                 robotName.name.toLowerCase().includes(query)
               )
-              .map(({avatar, name}) => (
+              .map(({avatar, name, stats}) => (
                 <Row key={name} data-testid='row'>
                   <Column>
-                    {" "}
                     <Avatar spacing={2} src={avatar} />
                   </Column>
-                  <Column>{name}</Column>
+                  <Column name={1}>{name}</Column>
+                  <Column>{stats.matches_played}</Column>
+                  <Column>{stats.matches_won}</Column>
+                  <Column>{stats.matches_tied}</Column>
+                  <Column>{stats.matches_lost}</Column>
+                  <Column>{stats.games_win_rate * 100}%</Column>
                 </Row>
               ))}
           </Tbody>
         </Table>
         <StyledButton>
-          <Link to='/botsubmit' data-testid='linkToBotSubmit'>
+          <Link
+            to='/botsubmit'
+            data-testid='linkToBotSubmit'
+            style={{color: "black", fontWeight: 500}}>
             Create bot
           </Link>
         </StyledButton>
-      </StyledEntryCard>
-    </EntryPage>
+      </Container>
+    </PadreContainer>
   );
 };
 export default RobotsLibrary;
