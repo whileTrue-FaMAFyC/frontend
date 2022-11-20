0ee2603828434937329f84e2db0235c2f43153cb
isra2021 <egancarry@gmail.com>
2022-11-20 11:46:55 -0300

    PYR-175 bugfix routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route element={<Private />}>
-          <Route path='/restore' element={<PasswordRestore />} />
           <Route path='/listgames' element={<ListMatches />} />
           <Route path='/botsubmit' element={<BotSubmitPage />} />
           <Route path='/gameconfig' element={<MatchConfigPage />} />
@@ -24,6 +23,7 @@ const App = () => {
           <Route path='/library' element={<Library />} />
           <Route path='/profile' element={<ProfilePage />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/avatarSubmit' element={<AvatarSubmit />} />
           <Route path='/*' element={<p>404</p>} />
         </Route>
         <Route element={<Public />}>
@@ -31,6 +31,7 @@ const App = () => {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/verify' element={<Verify />} />
+          <Route path='/restore' element={<PasswordRestore />} />
           <Route path='/*' element={<p>404</p>} />
         </Route>
       </Routes>
@@ -38,47 +39,3 @@ const App = () => {
   );
 };
 export default App;
-
-/*
-
-
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Private />}>
-          <Route path='/restore' element={<PasswordRestore />} />
-          <Route path='/listgames' element={<ListMatches />} />
-          <Route path='/botsubmit' element={<BotSubmitPage />} />
-          <Route path='/gameconfig' element={<MatchConfigPage />} />
-          <Route path='/simCreate' element={<CreateSimulation />} />
-          <Route path='/match/:match_id' element={<Match />} />
-          <Route path='/library' element={<Library />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path='/home' element={<Home />} />
-        </Route>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-
-        <Route path='/verify' element={<Verify />} />
-
-        <Route path='/avatarSubmit' element={<AvatarSubmit />} />
-      </Routes>
-    </BrowserRouter>
-    
-          <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/restore' element={<PasswordRestore />} />
-        <Route path='/listgames' element={<ListMatches />} />
-        <Route path='/botsubmit' element={<BotSubmitPage />} />
-        <Route path='/gameconfig' element={<MatchConfigPage />} />
-        <Route path='/verify' element={<Verify />} />
-        <Route path='/simCreate' element={<CreateSimulation />} />
-        <Route path='/avatarSubmit' element={<AvatarSubmit />} />
-        <Route path='/match/:match_id' element={<Match />} />
-        <Route path='/library' element={<Library />} />
-        <Route path='/profile' element={<ProfilePage />} />
-
-        {/* <Route path='/avatarSubmit' element={<AvatarSubmit />} /> */
