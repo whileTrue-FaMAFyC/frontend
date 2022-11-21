4b6c6860626d278e7795b651a1fc121be24bcbb3
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2022-11-21 16:29:48 -0300

    PYR-183 Imagenes en public

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -5,11 +5,11 @@ const Welcome = () => {
   return (
     <WelcomeImg
       style={{
-        backgroundImage: `url("https://imgs.search.brave.com/VeYbVrPVN-lcUrB1ADmGaK2TGhepR0WIA8xKEIUav9E/rs:fit:1200:1200:1/g:ce/aHR0cDovL3dhbGxw/YXBlcmNhdmUuY29t/L3dwL2xXUzlyOGMu/anBn")`,
+        backgroundImage: `url(welcomeBackground.webp)`,
       }}>
       <div>
         <img
-          src='https://imgs.search.brave.com/yqn9FkhmMTk1HLhuO32J-X8vSy-8DzeF_5pqWDhUZ5A/rs:fit:1024:1024:1/g:ce/aHR0cHM6Ly9jZG4z/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvcm9ib3QtY2Fy/dG9vbi1wYXJ0LTMv/NTEyLzMyLTEwMjQu/cG5n'
+          src='welcomeRobot.webp'
           width={150}
           height={150}
           alt={""}
